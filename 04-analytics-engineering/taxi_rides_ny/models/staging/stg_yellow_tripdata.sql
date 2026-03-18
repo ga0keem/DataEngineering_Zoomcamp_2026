f72ecace86545e4475c722b2fcea9fb73bcf645b
ga0keem <liswiw@hanyang.ac.kr>
2026-03-18 12:14:58 +0000

    initial models

---
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
@@ -0,0 +1,44 @@
+with tripdata as (
+
+    select *
+    from {{ source('nytaxi','yellow_tripdata') }}
+    where VendorID is not null
+
+),
+
+renamed as (
+
+    select
+        -- identifiers
+        cast(VendorID as integer) as vendorid,
+        cast(RatecodeID as integer) as ratecodeid,
+        cast(PULocationID as integer) as pickup_locationid,
+        cast(DOLocationID as integer) as dropoff_locationid,
+
+        -- timestamps
+        cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
+        cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime,
+
+        -- trip info
+        store_and_fwd_flag,
+        cast(passenger_count as integer) as passenger_count,
+        cast(trip_distance as numeric) as trip_distance,
+        cast(1 as integer) as trip_type,  -- Yellow는 항상 street-hail
+
+        -- payment info
+        cast(fare_amount as numeric) as fare_amount,
+        cast(extra as numeric) as extra,
+        cast(mta_tax as numeric) as mta_tax,
+        cast(tip_amount as numeric) as tip_amount,
+        cast(tolls_amount as numeric) as tolls_amount,
+        cast(0 as numeric) as ehail_fee,  -- Yellow에는 없음
+        cast(improvement_surcharge as numeric) as improvement_surcharge,
+        cast(total_amount as numeric) as total_amount,
+        cast(payment_type as integer) as payment_type,
+        cast(congestion_surcharge as numeric) as congestion_surcharge
+
+    from tripdata
+
+)
+
+select * from renamed
